72f2dcae846312b0599e066fee853635c72c18e8
gdoganieri <277168@studenti.unimore.it>
2021-12-13 14:19:04 +0100

    add visualization function

--- posenet/common/utils/vis.py
+++ posenet/common/utils/vis.py
@@ -116,6 +116,49 @@ def vis_3d_multiple_skeleton(kpt_3d, kpt_3d_vis, kps_lines, filename=None):
     cv2.waitKey(0)
 
 
+def vis_3d_multiple_skeleton_and_pointcloud(kpt_3d, kpt_3d_vis, kps_lines, filename=None, pointcloud=None):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Convert from plt 0-1 RGBA colors to 0-255 BGR colors for opencv.
+    cmap = plt.get_cmap('rainbow')
+    colors = [cmap(i) for i in np.linspace(0, 1, len(kps_lines) + 2)]
+    colors = [np.array((c[2], c[1], c[0])) for c in colors]
+
+    for l in range(len(kps_lines)):
+        i1 = kps_lines[l][0]
+        i2 = kps_lines[l][1]
+
+        person_num = kpt_3d.shape[0]
+        for n in range(person_num):
+            x = np.array([kpt_3d[n, i1, 0], kpt_3d[n, i2, 0]])
+            y = np.array([kpt_3d[n, i1, 1], kpt_3d[n, i2, 1]])
+            z = np.array([kpt_3d[n, i1, 2], kpt_3d[n, i2, 2]])
+
+            if kpt_3d_vis[n, i1, 0] > 0 and kpt_3d_vis[n, i2, 0] > 0:
+                ax.plot(x, z, -y, c=colors[l], linewidth=2)
+            if kpt_3d_vis[n, i1, 0] > 0:
+                ax.scatter(kpt_3d[n, i1, 0], kpt_3d[n, i1, 2], -kpt_3d[n, i1, 1], c=colors[l], marker='o')
+            if kpt_3d_vis[n, i2, 0] > 0:
+                ax.scatter(kpt_3d[n, i2, 0], kpt_3d[n, i2, 2], -kpt_3d[n, i2, 1], c=colors[l], marker='o')
+
+    if filename is None:
+        ax.set_title('3D vis')
+    else:
+        ax.set_title(filename)
+
+    if pointcloud is not None:
+        ax.scatter3D(pointcloud[:, 0], pointcloud[:, 2], pointcloud[:, 1], s=0.25)
+        # ax.view_init(10, 240)
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Z Label')
+    ax.set_zlabel('Y Label')
+    ax.legend()
+
+    plt.show()
+    cv2.waitKey(1)
+
 
 
 def vis_3d_multiple_skeleton_no_show_but_savefig(kpt_3d, kpt_3d_vis, kps_lines, filename=None):
